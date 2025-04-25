ba03cb5a0df2feb08256a3b1e95626c7d4bc93f9
User <user@pop-os.cablelynx.com>
2025-04-25 04:04:16 -0500

    someting

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import os,sys
 from pathlib import Path
 import subprocess
 import gdown
+import webbrowser
+
 
 
 root = Path(os.path.dirname( __file__ ))
@@ -22,9 +24,15 @@ if __name__ == "__main__":
     # ask if we need to pull files,
     urlID = "16eBcMpkgMTV9mlKxYmNda64X_dmCFdkk"
     url = "https://drive.google.com/drive/folders/16eBcMpkgMTV9mlKxYmNda64X_dmCFdkk?usp=drive_link"# "https://drive.google.com/drive/folders/" + urlID
-    
+    # https://drive.google.com/uc?id=16eBcMpkgMTV9mlKxYmNda64X_dmCFdkk&export=download
     # download to 'maps/' folder
     output_path = str(mapsFolder)
+    # webbrowser.register('firefox')
+    # webbrowser.open_new(url)
+    os.system(f"""firefox --new-window {url}""")
+    # webbrowser.open(url=url, new=1,)
+    
+
     # https://www.intodeeplearning.com/how-to-download-files-or-folders-in-gdrive-in-python/
     # downloadResponse = gdown.download_folder(url, output=mapsFolder, remaining_ok=True, quiet=True,skip_download=True)
     
@@ -34,14 +42,15 @@ if __name__ == "__main__":
     # https://stackoverflow.com/questions/38511444/python-download-files-from-google-drive-using-url
     # https://stackoverflow.com/questions/25010369/wget-curl-large-file-from-google-drive/39225039#39225039
     
-    gdown.download(url=url,\
-                    format="zip",\
-                    output=output_path,\
-                    verify=False,
-                    resume=False,\
-                    quiet=False,\
-                    fuzzy=True)
-    # print(downloadResponse)
+    # gdown.download_folder(url=url, output=output_path, remaining_ok=True)
+    # gdown.download(url=url,\
+    #                 format="zip",\
+    #                 output=output_path,\
+    #                 verify=False,
+    #                 resume=False,\
+    #                 quiet=False,\
+    #                 fuzzy=True)
+    # # print(downloadResponse)
     # get the zip files
     # https://stackoverflow.com/questions/1606795/catching-stdout-in-realtime-from-subprocess
     # formats the subprocess into a list of strings
